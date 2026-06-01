fcf453a9d19992bc06e09074d3e956cc6db470f6
brynlee160 <brynlee.crispin@uvm.edu>
2026-05-31 21:32:53 -0400

    basic organizational updates

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Brynlee Crispin on 5/31/26.
+//
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+using namespace std;
+
+int main() {
+
+
+
+}
---
+++ starting_page/start_main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Brynlee Crispin on 5/31/26.
+//
+
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+using namespace std;
+
+void run_starting_page() {
+
+}
