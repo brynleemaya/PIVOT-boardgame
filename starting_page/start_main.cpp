974ee8771bdcf47ebf32e7f783850f03a3b9aaf0
brynlee160 <brynlee.crispin@uvm.edu>
2026-05-31 21:36:19 -0400

    basic organizational updates

--- starting_page/start_main.cpp
+++ starting_page/start_main.cpp
@@ -5,9 +5,18 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <string>
 
 using namespace std;
 
 void run_starting_page() {
+
+    // Declare background image
+    string background = "../page_images/welcome.png";
+
+    // Create window
+    SDL_Window* window = SDL_CreateWindow("Communityland",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        );
 
 }
